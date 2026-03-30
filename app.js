3b6fbff7f742b351f419491485694a88d509990a
Kitsune7X <fox.dev7@proton.me>
2026-03-30 10:28:08 -0700

    health check

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
+  console.log('Someone pinged /version')
   res.send('1')
+})
+
+// Health Check
+app.get('/health', (_req, res) => {
+  console.log('Health check')
+  res.send('ok')
 })
 
 const start = async () => {
